72910a2378e697519cb11110b45ee83a80c56de7
Marcos jose <marcosjoseDV@hotmail.com>
2023-01-12 17:09:18 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿
+using DollarContacao.models;
+using Newtonsoft.Json;
+namespace DollarContacao;
+
+public class Porgram
+{
+    static async Task Main(string[] args)
+    {
+        var url = "https://economia.awesomeapi.com.br/last/";
+        using HttpClient client = new() { BaseAddress = new Uri(url) };
+        var response = await client.GetAsync("/USD-BRL");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var cotacaos = JsonConvert.DeserializeObject<Cotacao[]>(content);
+            foreach (var cotacao in cotacaos)
+            {
+                Console.WriteLine($"Name: {cotacao.Name} - {cotacao.Code}");
+                Console.WriteLine($"Máximo: {cotacao.High}");
+                Console.WriteLine($"Mínimo: {cotacao.Low}");
+                Console.WriteLine();
+            }
+
+            response = await client.GetAsync("/EUR-BRL");
+            content = await response.Content.ReadAsStringAsync();
+            cotacaos = JsonConvert.DeserializeObject<Cotacao[]>(content);
+            foreach (var cotacao in cotacaos)
+            {
+                Console.WriteLine($"Name: {cotacao.Name} - {cotacao.Code}");
+                Console.WriteLine($"Máximo: {cotacao.High}");
+                Console.WriteLine($"Mínimo: {cotacao.Low}");
+                Console.WriteLine();
+            }
+
+            response = await client.GetAsync("/BTC-BRL");
+            content = await response.Content.ReadAsStringAsync();
+            cotacaos = JsonConvert.DeserializeObject<Cotacao[]>(content);
+            foreach (var cotacao in cotacaos)
+            {
+                Console.WriteLine($"Name: {cotacao.Name} - {cotacao.Code}");
+                Console.WriteLine($"Máximo: {cotacao.High}");
+                Console.WriteLine($"Mínimo: {cotacao.Low}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error, Get falhou");
+        }
+    }
+}
---
+++ models/Cotacao.cs
@@ -0,0 +1,14 @@
+﻿namespace DollarContacao.models
+{
+    public class Cotacao
+    {
+        public string USDBRL { get; set; }
+        public string EURBRL { get; set; }
+        public string BTCBRL { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string High { get; set; }
+        public string Low { get; set; }
+        public string Data { get; set; }
+    }
+}
